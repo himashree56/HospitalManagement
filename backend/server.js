8a9c04fc9aef70269612612410b608c295f620b0
himashree56 <125894188+himashree56@users.noreply.github.com>
2025-04-03 00:29:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,7 @@ app.use('/api/patient', patientRoutes);
 app.use('/api/doctor', doctorRoutes);
 app.use('/api/admin', adminRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send("MedCorx API is running...");
+});
+export default app;
